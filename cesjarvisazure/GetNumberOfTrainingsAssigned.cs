dd9892c1ea8d2621296714d79608da19e43371f1
KLee <Klee@csod.com>
2017-09-11 23:17:30 -0700

    add more api methods

--- cesjarvisazure/GetNumberOfTrainingsAssigned.cs
+++ cesjarvisazure/GetNumberOfTrainingsAssigned.cs
@@ -2,18 +2,16 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Threading.Tasks;
-using Microsoft.Azure.WebJobs;
-using Microsoft.Azure.WebJobs.Extensions.Http;
-using Microsoft.Azure.WebJobs.Host;
-using System.Collections.Generic;
 using System;
 using Newtonsoft.Json.Linq;
+using Newtonsoft.Json.Serialization;
+using Newtonsoft.Json;
 
 namespace cesjarvisazure
 {
     public static class GetNumberOfTrainingsAssigned
     {
-        public static async Task<ApiAiResponse> GetTrainingMetrics(TraceWriter log, int userId, string bearerToken, string sessionIdToken)
+        public static async Task<ApiAiResponse> GetTrainingMetrics(ITraceWriter log, int userId, string bearerToken, string sessionIdToken)
         {
             //Get URL to execute
             var trainingMetricsUrl = TranscriptAPI.GetTrainingMetricsURL(userId);
@@ -42,6 +40,89 @@ namespace cesjarvisazure
             response.speech = responseText;
             return response;
 
+        }
+
+        public static async Task<ApiAiResponse> SearchName(ITraceWriter log, string searchTerms, string bearerToken, string sessionIdToken)
+        {
+            //Get URL to execute
+            var searchUrl = TranscriptAPI.SearchName(searchTerms);
+
+            //Execute URL and return results
+            string trainingMetrics = string.Empty;
+
+            ApiAiResponse response = new ApiAiResponse();
+            string responseText;
+            try
+            {
+                JObject searchresultobject = JObject.Parse(RequestHelper.ExecuteUrl(searchUrl, bearerToken, sessionIdToken));
+                JToken result = searchresultobject["data"].FirstOrDefault();
+                string personName = result[0]["FirstName"].ToString() + " " + result[0]["LastName"].ToString();
+                string personPhone = result[0]["PhoneWork"].ToString();
+
+                responseText = $"You have {personName} results.";
+            }
+            catch (Exception ex)
+            {
+                responseText = "Something went wrong. Please try again.";
+            }
+
+            response.displayText = responseText;
+            response.speech = responseText;
+            return response;
+        }
+
+        public static async Task<ApiAiResponse> CreatePosting(ITraceWriter log, int requisitionId, int careerSiteId, string bearerToken, string sessionIdToken)
+        {
+            //Get URL to execute
+            var postingUrl = TranscriptAPI.PostingURL(requisitionId);
+
+            //Execute URL and return results
+            string jsonStuff = JsonConvert.SerializeObject(new[] { new { careerSiteId = careerSiteId, startDate = DateTime.Now, endDate = DateTime.Now.AddDays(7), isDefault = true } });
+
+            ApiAiResponse response = new ApiAiResponse();
+            string responseText;
+            try
+            {
+                JObject postingobject = JObject.Parse(RequestHelper.ExecuteUrl(postingUrl, bearerToken, sessionIdToken, jsonStuff, HttpMethod.Put.Method));
+                JToken result = postingobject["data"].FirstOrDefault();
+                
+                responseText = $"Done.";
+            }
+            catch (Exception ex)
+            {
+                responseText = "Something went wrong. Please try again.";
+            }
+
+            response.displayText = responseText;
+            response.speech = responseText;
+            return response;
+        }
+
+        public static async Task<ApiAiResponse> RemovePosting(ITraceWriter log, int requisitionId, string bearerToken, string sessionIdToken)
+        {
+            //Get URL to execute
+            var postingUrl = TranscriptAPI.PostingURL(requisitionId);
+
+            //Execute URL and return results
+            string trainingMetrics = string.Empty;
+
+            ApiAiResponse response = new ApiAiResponse();
+            string responseText;
+            try
+            {
+                JObject postingobject = JObject.Parse(RequestHelper.ExecuteUrl(postingUrl, bearerToken, "[]", HttpMethod.Put.Method));
+                JToken result = postingobject["data"].FirstOrDefault();
+                
+                responseText = $"Done.";
+            }
+            catch (Exception ex)
+            {
+                responseText = "Something went wrong. Please try again.";
+            }
+
+            response.displayText = responseText;
+            response.speech = responseText;
+            return response;
         }
     }
 }
